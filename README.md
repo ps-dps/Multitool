c9d8d90d01466ad5109338dd28f9299deb3ecb85
PuckiSilver <pucki.silver@gmail.com>
2023-04-22 15:28:28 +0200

    ♻ Generate using beet plugin instead of bolt

--- README.md
+++ README.md
@@ -32,8 +32,30 @@ loot give @s loot ps-multitool:{material}_multitool
 ```
 Make sure to replace `{material}` with one of `golden`, `wooden`, `stone`, `diamond` or `netherite`.
 
+## **Adding or removing a Multitool**
+You can generate your **own Multitool** data pack simply by **configuring a config** and building it
+
+> This also works for modded tools but note that **some mods break data packs** so I won't guarantee compatability or support
+
+1. Install [Python](https://www.python.org/downloads/) and [beet](https://github.com/mcbeet/beet)<br>
+    _If Python is allready installed, you should just be able to run `pip install beet` to install beet_
+
+2. Clone or download the [GitHub repository](https://github.com/ps-dps/Multitool)
+
+3. Add or remove `materials` in [`multitool_config.yml`](multitool_config.yml) and configure `overwrites`, for mods where a tool isn't following vanilla's naming scheme
+    > You can easily find the namespace by looking at the item in your inventory after pressing **F3 + H**, it should be in the form `<namespace>:<material>_<tool>`
+
+4. Move to this folder with a terminal and run `beet`, it should create a folder called `build` with your data pack in it
+
 ---
 Check me out on other platforms:
 
-[<img src="https://docs.modrinth.com/img/logo.svg" height="40" width="40"/>](https://modrinth.com/user/PuckiSilver)
-[<img src="https://www.planetminecraft.com/images/layout/favicon-64.png" height="40" width="40"/>](https://www.planetminecraft.com/member/puckisilver)
+<a href="https://github.com/PuckiSilver" target="_blank">
+  <img src="https://github.githubassets.com/favicons/favicon-dark.svg" height="40" width="40"/>
+</a>
+<a href="https://modrinth.com/user/PuckiSilver" target="_blank">
+  <img src="https://docs.modrinth.com/img/logo.svg" height="40" width="40"/>
+</a>
+<a href="https://www.planetminecraft.com/member/puckisilver" target="_blank">
+  <img src="https://www.planetminecraft.com/images/layout/favicon-64.png" height="40" width="40"/>
+</a>
--- plugins/generate_multitool.py
+++ plugins/generate_multitool.py
@@ -1,4 +1,5 @@
-from beet import Context, FunctionTag, Function, ItemModifier, Predicate, BlockTag
+from PIL import Image
+from beet import Advancement, Context, ExtraPin, FunctionTag, Function, ItemModifier, ItemTag, LootTable, PngFile, Predicate, BlockTag, Recipe, Texture
 import yaml
 
 def beet_default(ctx: Context):
@@ -12,8 +13,7 @@ def beet_default(ctx: Context):
         except:
             print('[Multitool] Can\'t read Config')
             return
-    print(config)
-    return
+    if not generate_from_config(ctx, config): return
     add_lantern_load(ctx)
     add_multitool_base(ctx)
 
@@ -39,6 +39,7 @@ def add_lantern_load(ctx: Context):
     )
 
 def add_multitool_base(ctx: Context):
+    ctx.data.icon = Texture(Image.open('images/pack.png'))
     ctx.data.function_tags["load:load"] = FunctionTag({"values":["ps-multitool:load"]})
     ctx.data.functions["ps-multitool:load"] = Function([
         "scoreboard objectives add ps-multitool dummy",
@@ -64,10 +65,10 @@ def add_multitool_base(ctx: Context):
         "advancement revoke @s only ps-multitool:keep_dura",
         "item modify entity @s weapon.mainhand ps-multitool:keep_dura",
     ])
-    ctx.data.item_modifiers["ps-multitool:has_multitool"] = ItemModifier(
+    ctx.data.item_modifiers["ps-multitool:keep_dura"] = ItemModifier(
         {"function": "minecraft:copy_nbt","source": "this","ops": [{"source": "SelectedItem.tag.Damage","target": "Damage","op": "merge"}]}
     )
-    ctx.data.predicates["ps-multitool:keep_dura"] = Predicate({
+    ctx.data.predicates["ps-multitool:has_multitool"] = Predicate({
         "condition": "minecraft:alternative", "terms": [{ "condition": "minecraft:entity_properties", "entity": "this", "predicate": { "tool": "minecraft:player",
             "equipment": { "mainhand": {
                 "tag": "ps-multitool:tools",
@@ -76,13 +77,13 @@ def add_multitool_base(ctx: Context):
     ctx.data.predicates["ps-multitool:repair_chance"] = Predicate({
         "condition": "minecraft:random_chance", "chance": 0.7
     })
-    ctx.data.predicates["ps-multitool:should_hoe"] = Predicate([
-        { "condition": "minecraft:entity_properties", "entity": "this", "predicate": {
-            "flags": { "is_sneaking": True }}},
-        { "condition": "minecraft:location_check", "predicate": {
-            "block": { "tag": "ps-multitool:tillable" }}
-    }])
-    ctx.data.predicates["ps-multitool:block_tag"] = BlockTag({ "values": [
+    ctx.data.predicates["ps-multitool:should_hoe"] = Predicate("""[
+        { \"condition\": \"minecraft:entity_properties\", \"entity\": \"this\", \"predicate\": {
+            \"flags\": { \"is_sneaking\": true }}},
+        { \"condition\": \"minecraft:location_check\", \"predicate\": {
+            \"block\": { \"tag\": \"ps-multitool:tillable\" }}
+    }]""")
+    ctx.data.block_tags["ps-multitool:tillable"] = BlockTag({ "values": [
         { "id": "minecraft:grass_block", "required": False },
         { "id": "minecraft:dirt", "required": False },
         { "id": "minecraft:farmland", "required": False },
@@ -93,12 +94,82 @@ def add_multitool_base(ctx: Context):
 
 def generate_from_config(ctx: Context, cfg: dict):
     tools = ["axe", "hoe", "pickaxe", "shovel"]
-    for material in cfg['materials']:
-        ctx.data.functions[f'ps-multitool:craft/{material}_multitool'] = Function([
-            f'advancement revoke @s only ps-multitool:craft/{material}_multitool',
-            f'recipe take @s ps-multitool:{material}_multitool',
+    materials = cfg.get('materials')
+    if not materials:
+        print('[Multitool] The config doesn\'t specify any materials')
+        return False
+    for material in materials:
+        if len(material.split(':')) != 2:
+            print('[Multitool] Materials need to be in the form {namespace}:{material}')
+            return False
+    overwrites = cfg.get('overwrites', {})
+    for key, value in overwrites.items():
+        all_tools = [f'{material}_{tool}' for material in materials for tool in tools]
+        if not key in all_tools:
+            print('[Multitool] The key of each overwrite has to be {namespace}:{material}_{tool}')
+            return False
+        if value in all_tools:
+            print('[Multitool] The value of each overwrite can\'t be {namespace}:{material}_{tool}')
+            return False
+        if len(value.split(':')) != 2:
+            print('[Multitool] Overwrites need to be in the form {namespace}:{material}')
+            return False
+
+    for material in materials:
+        ctx.data.functions[f'ps-multitool:craft/{material.split(":")[1]}_multitool'] = Function([
+            f'advancement revoke @s only ps-multitool:craft/{material.split(":")[1]}_multitool',
+            f'recipe take @s ps-multitool:{material.split(":")[1]}_multitool',
             'clear @s knowledge_book',
-            f'loot give @s loot ps-multitool:{material}_multitool',
+            f'loot give @s loot ps-multitool:{material.split(":")[1]}_multitool',
         ])
+        ctx.data.item_tags[f'ps-multitool:material/{material.split(":")[1]}'] = ItemTag(
+            {"values": [{'id':f'{overwrites.get(material+"_"+tool,material+"_"+tool)}','required':False} for tool in tools]})
+        ctx.data.recipes[f'ps-multitool:{material.split(":")[1]}_multitool'] = Recipe({ "type": "minecraft:crafting_shapeless",
+            "ingredients": [{ "item": f'{overwrites.get(material+"_"+tool,material+"_"+tool)}' } for tool in tools ],
+            "result": { "item": "minecraft:knowledge_book" }})
+        ctx.data.loot_tables[f'ps-multitool:{material.split(":")[1]}_multitool'] = LootTable({ "pools": [{ "rolls": 1, "entries": [{ "type": "minecraft:item",
+            "name": f'{overwrites.get(material+"_pickaxe",material+"_pickaxe")}',
+            "functions": [{
+                "function": "minecraft:set_nbt",
+                "tag": "{ps-multitool:1b,CustomModelData:74201}" },
+            {
+                "function": "minecraft:set_lore",
+                "lore": [[
+                    { "text": "M", "color": "#00ff00", "italic": False },
+                    { "text": "u", "color": "#00e093", "italic": False },
+                    { "text": "l", "color": "#00baca", "italic": False },
+                    { "text": "t", "color": "#008fca", "italic": False },
+                    { "text": "i", "color": "#00639a", "italic": False },
+                    { "text": "t", "color": "#008fca", "italic": False },
+                    { "text": "o", "color": "#00baca", "italic": False },
+                    { "text": "o", "color": "#00e093", "italic": False },
+                    { "text": "l", "color": "#00ff00", "italic": False },
+        ]]}]}]}]})
+        ctx.data.advancements[f'ps-multitool:craft/{material.split(":")[1]}_multitool'] = Advancement({ "criteria": { "requirement": { "trigger": "minecraft:recipe_unlocked", "conditions": {
+            "recipe": f'ps-multitool:{material.split(":")[1]}_multitool' }}},
+            "rewards": { "function": f'ps-multitool:craft/{material.split(":")[1]}_multitool' }})
+
     for tool in tools:
-        ...
+        ctx.data.loot_tables[f'ps-multitool:swap/{tool}'] = LootTable(
+            {"pools":[{"rolls":1,"entries":[{
+                "type": "minecraft:item",
+                "name": f'{overwrites.get(material+"_"+tool,material+"_"+tool)}',
+                "conditions": [{ "condition": "minecraft:entity_properties", "entity": "this", "predicate": { "equipment": {
+                    "mainhand": {
+                        "tag": f'ps-multitool:material/{material.split(":")[1]}' }}}}],
+                "functions": [{ "function": "minecraft:copy_nbt", "source": "this", "ops": [{
+                    "source": "SelectedItem.tag",
+                    "target": "{}",
+                    "op": "merge" }]}]
+            } for material in materials ]}]})
+        ctx.data.item_tags[f'ps-multitool:tool/{tool}'] = ItemTag(
+            {"values": [{'id':f'{overwrites.get(material+"_"+tool,material+"_"+tool)}','required':False} for material in materials]})
+        ctx.data.predicates[f'ps-multitool:tool/{tool}'] = Predicate({
+            "condition": "minecraft:alternative", "terms": [{ "condition": "minecraft:entity_properties", "entity": "this", "predicate": { "tool": "minecraft:player", "equipment": {
+                "mainhand": {
+                    "tag": f'ps-multitool:tool/{tool}',
+                    "nbt": "{ps-multitool:1b}" }}}}]})
+
+    ctx.data.item_tags['ps-multitool:tools'] = ItemTag({ "values": [
+        { "id": f'#ps-multitool:tool/{tool}', "required": False } for tool in tools ]})
+    return True
